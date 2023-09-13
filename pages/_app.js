b815a73ac904344391da3362d7a5b9c535c98f61
Ilhan Derdiyok <ilhan.derdiyok@aicgroup.de>
2023-09-13 13:57:53 +0200

    added google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/inline-script-id */
 import { Provider } from "react-redux";
+import Script from "next/script";
 import store from "../store";
 import "../sass/style.scss";
 import { Outfit } from "@next/font/google";
@@ -8,19 +10,34 @@ const outfitRegular = Outfit({ weight: "400", subsets: ["latin"] });
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <style jsx global>{`
-    html {
-      font-family: ${outfitRegular.style.fontFamily};
-    }
-    h1 {
-      font-family: ${outfitBold.style.fontFamily};
-    }
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-  `}</style>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${outfitRegular.style.fontFamily};
+          }
+          h1 {
+            font-family: ${outfitBold.style.fontFamily};
+          }
+          h2,
+          h3,
+          h4,
+          h5,
+          h6,
+        `}
+      </style>
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
